7c5bcb840ca949acd716f0f5cf8c5b17a0d5afbb
danielmawasha <dmawasha@chieta.org.za>
2025-11-07 16:10:06 +0200

    chore-added application database context

---
+++ DataAccessLayer/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayer.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):DbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
